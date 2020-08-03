c406b374b971018b5a45ba27aee5da0b40047a39
StoneBearCap <69138785+StoneBearCap@users.noreply.github.com>
2020-08-03 16:34:45 +0800

    Add files via upload
    Main函数以及模拟测试演示。

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using CustomException;
+using CompetitorSetting;
+using CompetitionClass;
+
+namespace MyCompetition
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int AW = 0, BW = 0;
+            for(int i = 1; i <= 10; i++)
+            {
+                var A = new TheresaApocalypse();
+                var B = new KallenAndSakura();
+                var TCompetition = new Competition<TheresaApocalypse, KallenAndSakura>(A, B);
+                TCompetition.TaskFunction();
+                if(TCompetition.Winner == A)
+                {
+                    AW++;
+                }
+                else if(TCompetition.Winner == B)
+                {
+                    BW++;
+                }
+            }
+            Console.WriteLine($"{AW} {BW}");
+            Console.ReadLine();
+        }
+    }
+}
